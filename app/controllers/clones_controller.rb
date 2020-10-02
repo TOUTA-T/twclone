4bd108f19dde1a498c1303c6dc39de2b893ded3f
TOUTA-T <toutaby@aol.com>
2020-10-02 07:33:10 +0000

    画面遷移を追加

--- app/controllers/clones_controller.rb
+++ app/controllers/clones_controller.rb
@@ -1,20 +1,56 @@
 class ClonesController < ApplicationController
+    before_action :set_clone, only: [:show, :edit, :update, :destroy]
   def index
-    @clone =Clone.all
+    @clones = Clone.all
   end
   def new
-    @clone =Clone.new
+    @clone = Clone.new
   end
   def create
     @clone = Clone.new(clone_params)
-    if @clone.save
-      redirect_to new_clone_path, notice:"投稿しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @clone.save
+        redirect_to new_clone_path, notice:"投稿しました！"
+      else
+        render :new
+      end
     end
   end
+
+  def edit
+  end
+
+  def confirm
+    @clone = Clone.new(clone_params)
+    render :new if @clone.invalid?
+  end
+
+  def update
+    if @clone.update(clone_params)
+      redirect_to clones_path, notice:"投稿内容を編集しました"
+    else
+      render:edit
+    end
+  end
+
+  def destroy
+    @clone.destroy
+    redirect_to clones_path, notice:"ツイートを削除しました"
+  end
+
+  def show
+  end
+
   private
+
   def clone_params
     params.require(:clone).permit(:name,:content)
   end
+
+  def set_clone
+    @clone = Clone.find(params[:id])
+  end
+
 end
